5fa947a12cfb36aefb273157adae1d6d6b98b811
george-houen <u3324971@uni.canberra.edu.au>
2026-05-20 22:17:35 +1000

    feat: create class for main calculations

---
+++ calculator.py
@@ -0,0 +1,10 @@
+
+
+class CaclObject:
+    def __init__(self) -> None:
+        self._error_log : list[str] = []
+        pass
+    def full_calc_pipeline(self) -> dict[str, int] | None:
+        return 
+    def get_errors(self) -> list[str]:
+        return self._error_log
