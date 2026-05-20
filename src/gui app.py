1a24497a3b83379de288fee1a82a24ff44f72f49
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 00:56:20 +1000

    feat: import Any for type hinting

--- src/gui app.py
+++ src/gui app.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-
+from typing import Any
 
 class App(tk.Tk):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args : Any, **kwargs : Any):
         super().__init__(*args, **kwargs)
