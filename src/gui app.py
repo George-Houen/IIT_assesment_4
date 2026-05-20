afe3ac129c96b6e8df559f6dcba9e6d2396182b3
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 01:03:27 +1000

    feat: setup header and geometry

--- src/gui app.py
+++ src/gui app.py
@@ -5,6 +5,15 @@ class App(tk.Tk):
     def __init__(self, *args : Any, **kwargs : Any):
         super().__init__(*args, **kwargs)
 
+        #setup:
+        self.title("IIT assignment 4 - u3324971")
+        self.geometry("500x500")
+
+        self.header = tk.Frame(self)
+        self.header.grid(row=0, column=0)
+        tk.Label(self.header, text="IIT assignment 4").grid(column=0, row=0)
+        tk.Label(self.header, text="u3324971").grid(column=0, row=1)
+
 
 if __name__ == "__main__":
     App().mainloop()
