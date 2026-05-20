8b4392b1c7d789af2bebf117562da6372385cee4
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 01:09:10 +1000

    feat: added config file

---
+++ src/config.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+#directorys
+BASE_DIR = Path.cwd()
+
+#styles (family, size, "style1 style2 ...")
+HEADER_FONT = ("Arial", 24, "bold")
--- src/gui app.py
+++ src/gui app.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from typing import Any
+from config import (
+    HEADER_FONT
+)
 
 class App(tk.Tk):
     def __init__(self, *args : Any, **kwargs : Any):
@@ -11,7 +14,7 @@ class App(tk.Tk):
 
         self.header = tk.Frame(self)
         self.header.grid(row=0, column=0)
-        tk.Label(self.header, text="IIT assignment 4").grid(column=0, row=0)
+        tk.Label(self.header, text="IIT assignment 4", font=HEADER_FONT).grid(column=0, row=0)
         tk.Label(self.header, text="u3324971").grid(column=0, row=1)
 
 
