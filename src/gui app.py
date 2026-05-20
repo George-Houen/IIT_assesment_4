11b688556840df9e78044909915af8816f8ba1e8
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 00:57:24 +1000

    test: open GUI when file run

--- src/gui app.py
+++ src/gui app.py
@@ -4,3 +4,7 @@ from typing import Any
 class App(tk.Tk):
     def __init__(self, *args : Any, **kwargs : Any):
         super().__init__(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    App().mainloop()
