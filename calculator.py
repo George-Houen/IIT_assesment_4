84f5d71fb4846a7841a0a3d785cf89683896fd60
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 21:46:34 +1000

    feat: create function for monthly interest rate

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@
 class CaclObject:
     def __init__(self, 
                 loan_amount : float, 
-                interest_rate : float, 
+                interest_rate : float, #annual
                 monthly_expenses : float, 
                 loan_term : float, 
                 monthly_income : float
@@ -21,7 +21,18 @@ class CaclObject:
         self._total_interest : float | None = None
         self._monthly_cash_surplus : float | None = None
         pass
-    def full_calc_pipeline(self) -> dict[str, int] | None:
+    def get_monthly_interest_rate(self) -> float | None:
+        try:
+            if self._interest_rate < 0:
+                raise ValueError("interest rate cant be negative.")
+            monthly_i_rate = self._interest_rate / (100 * 12)
+        except Exception as e:
+            self._error_log.append(str(e))
+            return None
+        self._monthly_interest_rate = monthly_i_rate
+        return monthly_i_rate
+
+    def full_calc_pipeline(self) -> dict[str, float] | None:
         return 
     def get_errors(self) -> list[str]:
         return self._error_log
