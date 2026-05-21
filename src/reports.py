85d5abefe08cc9fab646e5ea7d26d3f6519b4fa6
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 23:24:33 +1000

    feat: tbh i have just confused myself at this point

--- src/reports.py
+++ src/reports.py
@@ -1,7 +1,7 @@
 from calculator import CalcObject
 from config import OUTPUT_DIR
 
-def generate_html(calc : CalcObject) -> None:
+def generate_html(inputs : dict[str, float], results : dict[str, float | str]) -> None:
     html = f"""
         <html>
             <head>
@@ -15,18 +15,71 @@ def generate_html(calc : CalcObject) -> None:
                 <h2>Input Values</h2>
     
                 <table border="1" cellpadding="5">
-
-
+                    <tr>
+                        <th>Input</th>
+                        <th>Value</th>
+                    </tr>
+    
+                    <tr>
+                        <td>Loan Amount</td>
+                        <td>${inputs["loan_amount"]:,.2f}</td>
+                    </tr>
+    
+                    <tr>
+                        <td>Interest Rate</td>
+                        <td>{inputs["interest_rate"]}%</td>
+                    </tr>
+    
+                    <tr>
+                        <td>Loan Term</td>
+                        <td>{inputs["loan_term"]} months</td>
+                    </tr>
+    
+                    <tr>
+                        <td>Monthly Income</td>
+                        <td>${inputs["monthly_income"]:,.2f}</td>
+                    </tr>
+    
+                    <tr>
+                        <td>Monthly Expenses</td>
+                        <td>${inputs["monthly_expenses"]:,.2f}</td>
+                    </tr>
                 </table>
     
                 <h2>Calculated Results</h2>
     
                 <table border="1" cellpadding="5">
-
+                    <tr>
+                        <th>Calculation</th>
+                        <th>Value</th>
+                    </tr>
+    
+                    <tr>
+                        <td>Monthly Repayment</td>
+                        <td>${results['monthly_repayment']:,.2f}</td>
+                    </tr>
+    
+                    <tr>
+                        <td>Total Repayment</td>
+                        <td>${results['total_repayment']:,.2f}</td>
+                    </tr>
+    
+                    <tr>
+                        <td>Total Interest</td>
+                        <td>${results['total_interest']:,.2f}</td>
+                    </tr>
+    
+                    <tr>
+                        <td>Monthly Cash Surplus</td>
+                        <td>${results['monthly_cash_surplus']:,.2f}</td>
+                    </tr>
                 </table>
     
                 <h2>Affordability Status</h2>
     
+                <p>
+                    <strong>{results['affordable']}</strong>
+                </p>
     
             </body>
         </html>
