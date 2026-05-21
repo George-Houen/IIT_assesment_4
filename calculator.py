bd83e74e23b6f5150b33c7bb87f11b0375786630
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 21:55:54 +1000

    feat: create function for monthly repayment

--- calculator.py
+++ calculator.py
@@ -20,7 +20,7 @@ class CaclObject:
         self._total_repayment : float | None = None
         self._total_interest : float | None = None
         self._monthly_cash_surplus : float | None = None
-        pass
+        
     def get_monthly_interest_rate(self) -> float | None:
         try:
             if self._interest_rate < 0:
@@ -31,9 +31,22 @@ class CaclObject:
             return None
         self._monthly_interest_rate = monthly_i_rate
         return monthly_i_rate
+    
+    def get_monthly_repayment(self) -> float | None:
+        try:
+            if self._loan_amount <= 0:
+                raise ValueError("loan amount must be more then 0") 
+            if self._monthly_interest_rate == None:
+                raise ValueError("monthly interest rate not calculated yet")
+            monthly_repayment = (self._loan_amount * self._monthly_interest_rate * (1+self._monthly_interest_rate) ** self._loan_term) / (((1+self._monthly_interest_rate) ** self._loan_term) - 1)
+        except Exception as e:
+            self._error_log.append(str(e))
+            return None
+        self._monthly_repayment = monthly_repayment
+        return monthly_repayment
 
     def full_calc_pipeline(self) -> dict[str, float] | None:
-        return 
+        return
     def get_errors(self) -> list[str]:
         return self._error_log
     
