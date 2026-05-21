dc84de788d5cdd986821fcc3bb2d0c5fc8ab488b
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 23:12:16 +1000

    feat: create file to handle file saving etc

---
+++ src/reports.py
@@ -0,0 +1,10 @@
+from calculator import CalcObject
+
+def generate_html(calc : CalcObject) -> None:
+    pass
+
+def generate_txt(calc : CalcObject) -> None:
+    pass
+
+def generate_sql(calc : CalcObject) -> None:
+    pass
