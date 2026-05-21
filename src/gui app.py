f358ce9bb2990cd39a4fbc66339df03f9fc04891
george-houen <u3324971@uni.canberra.edu.au>
2026-05-21 18:20:43 +1000

    feat: create body frame

--- src/gui app.py
+++ src/gui app.py
@@ -12,12 +12,17 @@ class App(tk.Tk):
         #setup:
         self.title("IIT assignment 4 - u3324971")
         self.geometry("500x500")
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
         self.header = tk.Frame(self, relief="raised", border=2)
         self.header.grid(row=0, column=0)
         tk.Label(self.header, text="IIT assignment 4", **HEADER).grid(column=0, row=0)
         tk.Label(self.header, text="u3324971", **SUB_HEADER).grid(column=0, row=1)
 
+        self.body = tk.Frame(self)
+        self.body.grid(row=1, column=0)
+        
 
 if __name__ == "__main__":
     App().mainloop()
